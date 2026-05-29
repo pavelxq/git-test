35eea75b1a3a36d5f9b57cafce8365dd11495371
yeyiqiang <yeyiqiang@xmyanqu.cn>
2026-05-29 17:50:47 +0800

    add

---
+++ ListHelper.java
@@ -0,0 +1,26 @@
+package com.yanqu.xiuxian.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListHelper {
+    /**
+     * 数值数组比较，忽略顺序
+     */
+    public static boolean integerListCompare(List<Integer> list1, List<Integer> list2){
+        if(null == list1 || null == list2){
+            return false;
+        }
+        if(list1.size() != list2.size()){
+            return false;
+        }
+        List<Integer> tempList = new ArrayList<>(list2);
+        for(Integer value : list1){
+            if(!tempList.contains(value)){
+                return false;
+            }
+            tempList.remove(value);
+        }
+        return true;
+    }
+}
